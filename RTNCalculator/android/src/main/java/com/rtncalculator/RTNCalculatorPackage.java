764caab4425d337f775c92e8a0bb3e8ff5823744
ismaelsousa <ismaelsousa1998@outlook.com>
2022-09-20 21:12:03 -0300

    compiled code

--- RTNCalculator/android/src/main/java/com/rtncalculator/RTNCalculatorPackage.java
+++ RTNCalculator/android/src/main/java/com/rtncalculator/RTNCalculatorPackage.java
@@ -5,20 +5,41 @@ import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.module.model.ReactModuleInfoProvider;
 import com.facebook.react.TurboReactPackage;
+import com.facebook.react.module.model.ReactModuleInfo;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
-public class CalculatorPackage extends TurboReactPackage {
+public class RTNCalculatorPackage extends TurboReactPackage {
 
   @Nullable
   @Override
   public NativeModule getModule(String name, ReactApplicationContext reactContext) {
+      if (name.equals(RTNCalculatorModule.NAME)) {
+          return new RTNCalculatorModule(reactContext);
+      } else {
           return null;
+      }
   }
 
   @Override
   public ReactModuleInfoProvider getReactModuleInfoProvider() {
-      return null;
+      return () -> {
+          final Map<String, ReactModuleInfo> moduleInfos = new HashMap<>();
+          moduleInfos.put(
+                  RTNCalculatorModule.NAME,
+                  new ReactModuleInfo(
+                          RTNCalculatorModule.NAME,
+                          RTNCalculatorModule.NAME,
+                          false, // canOverrideExistingModule
+                          false, // needsEagerInit
+                          true, // hasConstants
+                          false, // isCxxModule
+                          true // isTurboModule
+          ));
+          return moduleInfos;
+      };
   }
 }
